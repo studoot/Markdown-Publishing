07f9b77e093f7cf8a3878cba8e098556adacc255
Stuart Dootson <stuart.dootson@rolls-royce.com>
2020-10-05 16:02:44 +0100

    Made definition list filter more general

--- filters/fix-defn-lists.lua
+++ filters/fix-defn-lists.lua
@@ -3,10 +3,12 @@
     that we want to keep with next paragraph
 ]]
 
-function fix_notes(element)
+-- Presume that a paragraph with a single, bold element is a title, and should
+-- be kept with the next paragraph.
+function fix_title(element)
     if #element.content == 1 and element.content[1].tag == "Strong" then
         local strong = element.content[1].content
-        if #strong == 1 and strong[1].tag == "Str" and strong[1].text == "Notes" then
+        if #strong == 1 and strong[1].tag == "Str" then
             table.insert(element.content, pandoc.RawInline('latex', '\\nopagebreak'))
         end
     end
@@ -17,5 +19,5 @@ function DefinitionList(block)
     for k, v in pairs(block.content) do
         table.insert(v[1], pandoc.RawInline('latex', '\\nopagebreak'))
     end
-    return pandoc.walk_block(block, {Plain = fix_notes})
+    return pandoc.walk_block(block, {Plain = fix_title})
 end
